c01d022d3b7101e4ec9a68539cec9b1aa63eb43a
Belerafo <68774802+Belerafo@users.noreply.github.com>
2021-06-03 10:52:57 +0300

    in progress

---
+++ app/src/main/java/com/example/foodchest/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.foodchest
+
+class Constants {
+    companion object{
+
+        const val API_KEY = "9ba43328ace34e8ba833f99cf04d8e8b"
+        const val  BASE_URL = "https://api.spoonacular.com"
+
+    }
+}
