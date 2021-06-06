c5a40d90c6b4356200c1fc5415975e30ffa5a985
Lyshk <lyshka9@yandex.ru>
2021-06-06 14:47:11 +0300

    in progress

---
+++ app/src/main/java/com/example/foodchest/data/database/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package com.example.foodchest.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.foodchest.models.FoodRecipe
+import com.example.foodchest.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity (
+    var foodRecipe: FoodRecipe
+) {
+
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+
+}
