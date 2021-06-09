c4a5e443472695da957db4a92335f1085ef73052
Lyshk <lyshka9@yandex.ru>
2021-06-09 20:07:23 +0300

    in progress

--- app/src/main/java/com/example/foodchest/bindingadapter/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foodchest/bindingadapter/RecipesRowBinding.kt
@@ -1,16 +1,36 @@
 package com.example.foodchest.bindingadapter
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.foodchest.R
+import com.example.foodchest.models.Result
+import com.example.foodchest.ui.fragments.recipes.RecipesFragmentDirections
 import org.w3c.dom.Text
+import java.lang.Exception
 
 class RecipesRowBinding {
     companion object{
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipesClickListener(recipeRowLayout: ConstraintLayout, result: Result){
+            recipeRowLayout.setOnClickListener{
+                try{
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                }catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
