ebf97b3c5eb75afd4061f19902b66f99d94e7a2e
Belerafo <68774802+Belerafo@users.noreply.github.com>
2021-06-16 14:31:48 +0300

    in progress

--- app/src/main/java/com/example/foodchest/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/foodchest/ui/DetailsActivity.kt
@@ -19,6 +19,7 @@ import com.example.foodchest.ui.fragments.instructions.InstructionsFragment
 import com.example.foodchest.ui.fragments.overview.OverviewFragment
 import com.example.foodchest.viewmodels.MainViewModel
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -55,15 +56,19 @@ class DetailsActivity : AppCompatActivity() {
         val resultBundle = Bundle()
         resultBundle.putParcelable("recipeBundle", args.result)
 
-        val adapter = PagerAdapter(
+        val pagerAdapter = PagerAdapter(
             resultBundle,
             fragments,
-            titles,
-            supportFragmentManager
+            this
         )
 
-        binding.viewPager.adapter = adapter
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
+        binding.viewPager2.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2){ tab, position ->
+            tab.text = titles[position]
+        }.attach()
 
     }
 
